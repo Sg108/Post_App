dd6096b995c64ccc317799c4afabe9594514a286
Shivam <guptashivamgzb@gmail.com>
2024-03-08 19:41:04 +0530

    reg3

--- src/components/Auth/Layout.jsx
+++ src/components/Auth/Layout.jsx
@@ -6,14 +6,14 @@ import Login from './Login'
 //import Login from './Login'
 function Layout(props) {
     console.log(localStorage.getItem('reg'))
-    const [reg,setReg] = useState(localStorage.getItem('reg')===undefined?true:localStorage.getItem('reg'))
+    const [reg,setReg] = useState(localStorage.getItem('reg')===undefined?'true':localStorage.getItem('reg'))
     console.log(reg)
   return (
     <div className='w-screen h-screen bg-blue-200 relative overflow-hidden'>
        <Heading text="WELCOME TO POST APP"/>
-       {reg===true && (<><Register setAuthenticated={props.setAuthenticated}/>
+       {reg==='true' && (<><Register setAuthenticated={props.setAuthenticated}/>
          <div className='  mx-auto w-fit mt-2'>
-         <motion.button onClick={()=>{localStorage.setItem('reg',false);setReg(false)}} whileTap={{ scale: 0.8 }} className=' text-slate-600 text-[12px] '>
+         <motion.button onClick={()=>{localStorage.setItem('reg','false');setReg('false')}} whileTap={{ scale: 0.8 }} className=' text-slate-600 text-[12px] '>
          <p className='underline underline-offset-1'>already registered,</p>
           <p className='underline underline-offset-1'>click here to login</p>
          
@@ -21,9 +21,9 @@ function Layout(props) {
          </div> 
          </>
       )}
-       {reg===false && (<><Login  setAuthenticated={props.setAuthenticated} setReg={setReg}/>
+       {reg==='false' && (<><Login  setAuthenticated={props.setAuthenticated} setReg={setReg}/>
        <div className='  mx-auto w-fit mt-2'>
-       <motion.button onClick={()=>{localStorage.setItem('reg',true);setReg(true) }} whileTap={{ scale: 0.8 }} className=' text-slate-600 text-[12px] '>
+       <motion.button onClick={()=>{localStorage.setItem('reg','true');setReg('true') }} whileTap={{ scale: 0.8 }} className=' text-slate-600 text-[12px] '>
        <p className='underline underline-offset-1'>Not registered yet,</p>
         <p className='underline underline-offset-1'>click here to register</p>
        
