b23f4fcb1b45b856be90e9407243ecab3195ba5b
Shivam <guptashivamgzb@gmail.com>
2024-03-12 15:45:11 +0530

    header padding

--- src/components/Auth/Heading.jsx
+++ src/components/Auth/Heading.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion"
 function Heading(props) {
   //console.log(props.text)
   return (
-    <div   className="flex font-mono w-fit relative md:text-5xl text-3xl text-black z-20 mx-auto p-5">
+    <div   className="flex font-mono w-fit relative md:text-5xl text-3xl text-black z-20 mx-auto pt-5 pb-5">
        { props.text.split('').map((x,i)=>{
         if(x===' ')
         return <div key={i}>&nbsp;</div>
